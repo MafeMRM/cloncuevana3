10ae732580487bc3ca03039726bd6043fa0f28d4
MafeMRM <fernanda.moreno@est.iudigital.edu.co>
2025-10-05 22:31:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const {verifyToken} = require('./utils/auth');
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 // Importar rutas
 const authRoutes = require('./routes/authRoutes');
 const directorRoutes = require('./routes/directorRoutes');
